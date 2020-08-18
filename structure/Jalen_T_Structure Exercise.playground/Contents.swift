cc2e158d71b9cabe713ae0b84655e131fba7990a
jalen-cryptoHack <67969173+jalen-cryptoHack@users.noreply.github.com>
2020-08-18 07:05:01 -0500

    Add files via upload

---
+++ structure/Jalen_T_Structure Exercise.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+struct car_engine{
+    var size = "v8"
+    var size2 = "V12"
+    var size3 = "I-6"
+}
+let mycar = car_engine()
+
+print("when getting this car you could get this engine: \(mycar.size)")
+print("when getting this car you could get this engine: \(mycar.size2)")
+print("when getting this car you could get this engine: \(mycar.size3)")
